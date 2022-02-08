e555c195c253f96a033b103338f1758462a12902
guzov-afk <55351226+guzov-afk@users.noreply.github.com>
2022-02-08 11:10:50 +0200

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,55 @@
+import gc
+import machine
+import network
+import upip
+
+
+def connect_wlan(ssid, password):
+    """Connects build-in WLAN interface to the network.
+    Args:
+        ssid: Service name of Wi-Fi network.
+        password: Password for that Wi-Fi network.
+    Returns:
+        True for success, Exception otherwise.
+    """
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    sta_if.active(True)
+    ap_if.active(False)
+
+    if not sta_if.isconnected():
+        print("Connecting to WLAN ({})...".format(ssid))
+        sta_if.active(True)
+        sta_if.connect(ssid, password)
+        while not sta_if.isconnected():
+            pass
+
+    return True
+
+    """Main function. Runs after board boot, before main.py
+    Connects to Wi-Fi and checks for latest OTA version.
+    """
+
+gc.collect()
+gc.enable()
+
+    # Wi-Fi credentials
+SSID = "EXP"
+PASSWORD = "201060dx"
+
+connect_wlan(SSID, PASSWORD)
+
+    # Install Senko from PyPi
+upip.install("micropython-senko")
+
+import senko
+GITHUB_URL = "https://github.com/guzov-afk/esp32/blob/main/"
+OTA = senko.Senko(url=GITHUB_URL,files=["boot.py","main.py"])
+
+if OTA.update():
+    print("Updated to the latest version! Rebooting...")
+    machine.reset()
+
+
+
+
