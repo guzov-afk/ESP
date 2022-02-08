46afc4169dd1c40ff38ad1beac13b4b7d2611b73
guzov-afk <55351226+guzov-afk@users.noreply.github.com>
2022-02-08 12:02:48 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,41 +1,56 @@
-def connectToWifiAndUpdate():
-    import time, machine, network, gc
-    time.sleep(1)
-    print('Memory free', gc.mem_free())
+import gc
+import machine
+import network
+import upip
 
-    from app.ota_updater import OTAUpdater
 
+def connect_wlan(ssid, password):
+    """Connects build-in WLAN interface to the network.
+    Args:
+        ssid: Service name of Wi-Fi network.
+        password: Password for that Wi-Fi network.
+    Returns:
+        True for success, Exception otherwise.
+    """
     sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    sta_if.active(True)
+    ap_if.active(False)
+
     if not sta_if.isconnected():
-        print('connecting to network...')
+        print("Connecting to WLAN ({})...".format(ssid))
         sta_if.active(True)
-        sta_if.connect("EXP", "201060dx")
+        sta_if.connect(ssid, password)
         while not sta_if.isconnected():
             pass
-    print('network config:', sta_if.ifconfig())
-    otaUpdater = OTAUpdater('https://github.com/guzov-afk/ESP/edit/main/', files = "boot.py")
-    hasUpdated = otaUpdater.install_update_if_available()
-    if hasUpdated:
+
+    return True
+
+
+def main():
+    """Main function. Runs after board boot, before main.py
+    Connects to Wi-Fi and checks for latest OTA version.
+    """
+
+    gc.collect()
+    gc.enable()
+
+    # Wi-Fi credentials
+    SSID = ""
+    PASSWORD = ""
+
+    connect_wlan("EXP", "201060dx")
+
+    # Install Senko from PyPi
+    upip.install("micropython-senko")
+
+    import senko
+    OTA = senko.Senko(user="guzu-afk", repo="ESP", working_dir="main", files=["main.py"])
+
+    if OTA.update():
+        print("Updated to the latest version! Rebooting...")
         machine.reset()
-    else:
-        del(otaUpdater)
-        gc.collect()
 
 
-
-
-connectToWifiAndUpdate()
-
-import machine
-import time
-
-led = machine.Pin(2,machine.Pin.OUT)
-
-while True:
-    led.value(1)
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
